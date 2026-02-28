7bc1d877698e952cdd55850b25643bcae2276a83
Teddy Paz <pazteddy@gmail.com>
2026-02-28 14:18:59 -0400

    Add columns for name, price, and stock to products table migration

--- database/migrations/2026_02_28_171235_create_products_table.php
+++ database/migrations/2026_02_28_171235_create_products_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->decimal("price", 10, 2);
+            $table->unsignedInteger("stock");
             $table->timestamps(); //created_at, updated_at
         });
     }
