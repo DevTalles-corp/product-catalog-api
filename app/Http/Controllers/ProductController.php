391eb9674dcaf982c55594ab1e19e9049b9f547e
Teddy Paz <pazteddy@gmail.com>
2026-03-03 13:35:37 -0400

    Implement product creation with validation and response in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,7 +33,16 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "name" => ["required", "string", "min:2"],
+            "price" => ["required", "numeric", "min:0.01"],
+            "stock" => ["required", "integer", "min:0"]
+        ]);
+        $product = Product::create($validated);
+        return response()->json([
+            "message" => "Producto creado satisfactoriamente",
+            "data" => $product
+        ], 201);
     }
 
     /**
